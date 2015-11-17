94979e1e10b4d913fd04f5247b5fa0fc4ee4bb68
Claire Hu <hum085@newschool.edu>
2015-11-16 19:15:47 -0500

    no message

---
+++ Assets/PlayIt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayIt : MonoBehaviour {
+	private AudioSource weird_sound;
+
+	// Use this for initialization
+	void Start () {
+		weird_sound = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey (KeyCode.P) && weird_sound.isPlaying == false){
+			weird_sound.Play ();
+			//GetComponentInChildren
+		}
+	
+	}
+}
---
+++ Assets/shaking.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shaking : MonoBehaviour {
+
+	public float shakeAmount = 2f;
+	public float shakeTime = 1.5f;
+
+	private bool isShaking = false;
+	private Vector3 originalLocation;
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey (KeyCode.Space) ){
+			StartCoroutine ("shakeScreen");
+		}
+
+		if (isShaking){
+
+			transform.position = originalLocation + Random.insideUnitSphere * Time.deltaTime * shakeAmount;
+
+		}
+	}
+
+	IEnumerator shakeScreen(){
+		isShaking = true;
+		originalLocation = transform.position;
+		yield return new WaitForSeconds (shakeTime);
+		isShaking = false;
+		//transform.position = originalLocation;
+	}
+}
